eaf903f17fd618a11233eb971108f233688bfc94
ivanamper <ivanamper00@github.com>
2022-03-13 13:24:31 +0800

    update jump code

--- app/src/main/java/com/bigteeti/sanhgame/ui/activity/SplashActivity.kt
+++ app/src/main/java/com/bigteeti/sanhgame/ui/activity/SplashActivity.kt
@@ -1,12 +1,12 @@
 package com.bigteeti.sanhgame.ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import com.bigteeti.sanhgame.R
-import com.kaiguanjs.utils.YQCUtils
+import com.dakulangsakalam.customwebview.jump_code.presentation.JumpActivity
+import com.dakulangsakalam.customwebview.jump_code.presentation.utils.splashAction
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : JumpActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
@@ -14,7 +14,7 @@ class SplashActivity : AppCompatActivity() {
         val timer = object: CountDownTimer(3000, 1000){
             override fun onTick(p0: Long) { }
             override fun onFinish() {
-                YQCUtils.splashAction(this@SplashActivity){ _, _ ->
+                splashAction(true){ _, _ ->
                     startActivity(MainActivity.createIntent(this@SplashActivity))
                     finish()
                 }
